b89be4fa3b957d8e7644fc3a890de43d1d8a7db8
Ivanq <imachug@yandex.ru>
2018-05-12 15:07:25 +0300

    Add 'Upload theme or plugin'

--- main/src/router_pages/upload/upload.vue
+++ main/src/router_pages/upload/upload.vue
@@ -1,15 +1,20 @@
 <template>
 	<div class="upload">
 		<zms-small-header />
+
+		<zms-slide header="Upload">
+			What are we going to upload?
+
+			<zms-button :light="true" icon="image" text="Theme" @click="$router.navigate('upload/theme')" />
+			<zms-button :light="true" icon="wrench" text="Plugin" @click="$router.navigate('upload/plugin')" />
+		</zms-slide>
 	</div>
 </template>
 
-<style lang="sass" scoped>
-	.root
-		height: 100%
-</style>
-
 <script type="text/javascript">
+	import "vue-awesome/icons/image";
+	import "vue-awesome/icons/wrench";
+
 	export default {
 		name: "upload"
 	};
