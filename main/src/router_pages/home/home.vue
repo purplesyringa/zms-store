f77ffcc81475d0e0c76b78f867580e9eece54063
Ivanq <imachug@yandex.ru>
2018-05-12 14:34:12 +0300

    Add slides

--- main/src/router_pages/home/home.vue
+++ main/src/router_pages/home/home.vue
@@ -1,14 +1,35 @@
 <template>
-	<zms-header />
+	<div class="root">
+		<zms-header />
+
+		<zms-slide header="Themes">
+			Choose any theme you like for your blog.
+		</zms-slide>
+
+		<zms-slide header="Plugins">
+			Enchat your blog the way you like. Add comments, votes, "follow" button, integration with ZeroMe or whatever. With the use of plugins, you can even turn your blog into a public board.
+		</zms-slide>
+
+		<zms-slide header="Community">
+			<i>Community is everything.</i> Themes and plugins are all made by the community for the community. Make your own plugin or theme and upload it to ZMS Store or help us by <a href="https://github.com/imachug/zms">developing ZMS</a>!
+		</zms-slide>
+	</div>
 </template>
+
+<style lang="sass" scoped>
+	.root
+		height: 100%
+</style>
 
 <script type="text/javascript">
 	import Header from "./header.vue";
+	import Slide from "./slide.vue";
 
 	export default {
 		name: "home",
 		components: {
-			"zms-header": Header
+			"zms-header": Header,
+			"zms-slide": Slide
 		}
 	};
 </script>
---
+++ main/src/router_pages/home/slide.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="slide">
+		<header>{{header}}</header>
+		<div class="content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style lang="sass" scoped>
+	.slide
+		margin: 32px 128px
+		text-align: center
+
+	header
+		display: inline-block
+		margin-bottom: 32px
+		padding: 0 16px
+
+		font-family: Lobster, Verdana, Arial, sans-serif
+		font-size: 64px
+
+		border-bottom: 1px solid #CCC
+
+	.content
+		display: block
+
+		font-family: Verdana, Arial, sans-serif
+		font-size: 16px
+		text-align: left
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: "zms-slide",
+		props: ["header"]
+	};
+</script>
