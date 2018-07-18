d76def23541acd06b0ff8390dcd7e9cf69ddd270
Ivanq <imachug@yandex.ru>
2018-07-18 09:26:02 +0300

    Add more plugins

--- main/src/libs/theme-processor/js.js
+++ main/src/libs/theme-processor/js.js
@@ -2,6 +2,15 @@ import {transform} from "@babel/standalone/src";
 
 export function transformJs(path, code) {
 	return transform(code, {
-		presets: ["es2015"]
+		presets: ["es2015", "es2016", "es2017", "stage-0"],
+		plugins: [
+			[
+				require("babel-plugin-transform-builtin-extend"), {
+					globals: ["Error", "Array"]
+				}
+			],
+			require("babel-plugin-transform-class-properties"),
+			require("babel-plugin-transform-object-rest-spread")
+		]
 	}).code;
 }
